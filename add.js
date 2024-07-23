a31a865d2dfa9a31d8e36c5bab9d2ac68b7bf353
Syeda-Rohab <syedarohab8@gmail.com>
2024-07-23 02:49:12 -0700

    Add files via upload

---
+++ add.js
@@ -0,0 +1,11 @@
+"use strict";
+// operators ;
+Object.defineProperty(exports, "__esModule", { value: true });
+// index ;
+let index = 50;
+++index; // 50 + 1 ;
+--index; // 51 - 1 ;
+++index; // 50 + 1 ;
+--index; // 51 + 1 ;
+++index; // 50 + 1 ;
+console.log(index);
---
+++ add.ts
@@ -0,0 +1,18 @@
+// operators ;
+
+// index ;
+
+let index = 50 ;
+
+++ index // 50 + 1 ;
+
+-- index // 51 - 1 ;
+
+++ index // 50 + 1 ;
+
+-- index // 51 + 1 ;
+
+++ index // 50 + 1 ;
+
+console.log( index ) ;
+
